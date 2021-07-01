0515511ce9f1e71506fe62c2e3d783d87b7d5711
MrCezar <m.theus5423@gmail.com>
2021-07-01 01:05:45 -0300

    Added room page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { useHistory } from 'react-router-dom';
 
-import { auth, firebase } from '../services/firebase'
+import { auth, database, firebase } from '../services/firebase'
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -8,13 +8,15 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button'
 
 import '../styles/auth.scss';
-import { useContext } from 'react';
+import { FormEvent, useContext, useState } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import { useAuth } from '../hooks/useAuth';
 
 export function Home() {
     const history = useHistory();
     const { user, signInWithGoogle } = useAuth();
+    const [roomCode, setRoomCode] = useState('');
+
     async function handleCreateRoom() {
 
         if (!user)
@@ -22,6 +24,22 @@ export function Home() {
 
 
         history.push('/rooms/new');
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() == '')
+            return;
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('Room does not exist');
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`);
     }
 
     return (
@@ -39,10 +57,12 @@ export function Home() {
                         Crie a sua sala com o Google
                     </button>
                     <div className="separator">ou entre em uma sala</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input
                             type="text"
                             placeholder="Digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">
                             Entrar na sala
